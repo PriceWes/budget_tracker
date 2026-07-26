73571ef3433b80baba46102360cc03912518e4ad
PriceWes <obonyowesliy@gmail.com>
2026-07-26 21:42:41 +0300

    debugging

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,24 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    family: 4,
 });
 
 const sendEmail =async (to, subject, html) => {
-    await transporter.sendMail({
+    const info = await transporter.sendMail({
         from: `"Budget Tracker" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html,
     });
+    console.log(info.messageId);
 };
 
 export default sendEmail;
