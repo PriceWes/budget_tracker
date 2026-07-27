0b4f082b7f14dcea840a3d495becbfe0fea98bc0
PriceWes <obonyowesliy@gmail.com>
2026-07-27 09:37:25 +0300

    switch from gmail smtp to brevo

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,24 +1,22 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    family: 4,
 });
 
-const sendEmail =async (to, subject, html) => {
-    const info = await transporter.sendMail({
+const sendEmail = async (to, subject, html) => {
+    await transporter.sendMail({
         from: `"Budget Tracker" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html,
     });
-    console.log(info.messageId);
 };
 
 export default sendEmail;
